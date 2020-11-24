6c5555bf75824d2f99af08ba67a1ddc16c894314
kmkmkmkm49 <keitamuroga0409@gmail.com>
2020-11-24 12:04:08 +0900

    aiueo

--- Camera/js/json.js
+++ Camera/js/json.js
@@ -1,7 +1,7 @@
 $(function() {
     $('#getBookInfo').click( function( e ) {
         e.preventDefault();
-        const isbn = $("#scanned-QR").text();
+        const isbn = $("#scanned-QR").val();
         sessionStorage.setItem('isbn_key', isbn);
     });
 });
