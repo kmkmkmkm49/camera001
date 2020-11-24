ba25993ec96df6d7b1d9857c48a55b3e702cc0da
kmkmkmkm49 <keitamuroga0409@gmail.com>
2020-11-24 14:31:37 +0900

    aiueo

--- Camera/js/isbn_load.js
+++ Camera/js/isbn_load.js
@@ -1,7 +1,7 @@
 $(document).ready( function(){
     // ページ読み込み時に実行したい処理
     const isbn = sessionStorage.getItem('isbn_key');
-    const url = "/book_data/ " + isbn + ".json";
+    const url = "/book_data/" + isbn + ".json";
 
     $.getJSON( url, function( data ) {
         if( data == null ) {
@@ -12,12 +12,11 @@ $(document).ready( function(){
             } else {
                 $("#thumbnail").html('<img src=\"' + data.画像URL + '\" style=\"border:solid 1px #000000\" />');
             }
-            $("#isbn").val(data.ISBN);
-            $("#title").val(data.タイトル);
-            $("#subtitle").val(data.サブタイトル);
-            $("#authors").val(data.著者);
-            $("#publisher").val(data.出版社);
-            $("#exp").val(data.説明)
+            $("#title").text(data.タイトル);
+            $("#subtitle").text(data.サブタイトル);
+            $("#authors").text(data.著者);
+            $("#publisher").text(data.出版社);
+            $("#exp").text(data.説明)
         }
     }); 
 });
